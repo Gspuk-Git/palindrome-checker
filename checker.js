f49cf25f2cbfc04e42b54ccb34ae8e86ec016dd8
Gspuk-Git <101759187+Gspuk-Git@users.noreply.github.com>
2022-03-29 11:32:36 +1300

    Initial commit of completed project

---
+++ checker.js
@@ -0,0 +1,52 @@
+function palindrome(str) {
+    //regexpressions to test for characters
+    const reg = /[a-z0-9]/i;
+    const accReg = /[À-ÿ]/i;
+    //accented characters object
+    const accChars = {
+      à: 'a',
+      â: 'a',
+      ç: 'c',
+      é: 'e',
+      è: 'e',
+      ê: 'e',
+      ë: 'e',
+      î: 'i',
+      ï: 'i',
+      ô: 'o',
+      û: 'u',
+      ù: 'u',
+      ü: 'u'
+    }
+
+    let strArr = str
+                    .toLowerCase()
+                    .split('');
+    
+    function removeNonAlphaNum(arr){
+      const removedArr = [];
+      arr.forEach(character => {
+        if (reg.test(character)){
+          removedArr.push(character);
+        } else if (accReg.test(character)){
+          removedArr.push(accChars[character]);
+        }
+      })
+      return removedArr;
+    }
+    
+    //function to check if the array and reversed array are the same
+    function checkPali(arr,reversedArr){
+      return Array.isArray(arr) &&
+              Array.isArray(reversedArr) &&
+              arr.length === reversedArr.length &&
+              arr.every((val, index) => val === reversedArr[index]);
+    }
+    
+    strArr = removeNonAlphaNum(strArr);
+    const reversedArr = strArr.slice().reverse();
+    return checkPali(strArr, reversedArr);
+  
+  }
+  
+  palindrome("CÀmes");
